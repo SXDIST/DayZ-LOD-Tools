71845aa64ff10b26a144400aded661d388d9d94f
Akuma Alvarez <115116440+SXDIST@users.noreply.github.com>
2024-01-29 20:22:02 +0300

    The menu in the N Panel has been updated.
    The menu in the N Panel has been updated. Now there are 3 presets.  For Quads, Tris and Custom.  The first two are locked, and Custom allows you to change the values.

--- DayZLODTools.py
+++ DayZLODTools.py
@@ -11,97 +11,107 @@ bl_info = {
     "support": "COMMUNITY",
 }
 
+
 import bpy
+
+class LODPanelProperties(bpy.types.PropertyGroup):
+    use_change_names: bpy.props.BoolProperty(name="Change Names", default=True)
+    preset: bpy.props.EnumProperty(
+        name="Preset",
+        items=[
+            ('FOR_QUADS', "For Quads", "Create LODs for quadrangulate objects"),
+            ('FOR_TRIS', "For Tris", "Create LODs for triangulate objects"),
+            ('CUSTOM', "Custom", "Create LODs with custom settings"),
+        ],
+        default='FOR_QUADS',
+    )
+    decimate_values: bpy.props.FloatVectorProperty(
+        name="Decimate Values",
+        size=4,
+        min=0.0,
+        max=1.0,
+        default=(0.9, 0.7, 0.5, 0.3),
+    )
+
+    def create_lods(self, context, ratios, mode):
+        if not context.selected_objects:
+            self.report({'INFO'}, 'No object selected. Please select an object.')
+            return {'CANCELLED'}
+
+        original_obj = context.active_object
+        original_obj.name = 'LOD0'
+
+        for i, ratio in enumerate(ratios):
+            bpy.ops.object.duplicate(linked=False, mode='TRANSLATION')
+            new_obj = context.active_object
+
+            for modifier in new_obj.modifiers:
+                new_obj.modifiers.remove(modifier)
+
+            decimate_modifier = new_obj.modifiers.new(name=f'Decimate_{i}', type='DECIMATE')
+            decimate_modifier.ratio = ratio
+
+            weighted_normal_modifier = new_obj.modifiers.new(name=f'Weighted_Normal_{i}', type='WEIGHTED_NORMAL')
+            weighted_normal_modifier.keep_sharp = True
+            weighted_normal_modifier.use_face_influence = True
+
+            if self.use_change_names:
+                new_obj.name = f'LOD{i + 1}'
+
+        return {'FINISHED'}
 
 class OBJECT_PT_DayZLODToolsPanel(bpy.types.Panel):
     bl_label = "DayZ LOD Tools"
     bl_idname = "PT_DayZ_LOD_Tools_Panel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'Tools'
+    bl_category = 'LOD Tools'
     bl_context = "objectmode"
 
     def draw(self, context):
         layout = self.layout
-        row = layout.row()
-        row.operator("object.create_lods_quadrangulate")
+        box = layout.box()	
+        row = box.row()
+        row.prop(context.scene.lod_panel, "preset", text="")
 
-        row = layout.row()
-        row.operator("object.create_lods_triangulate")
+        row = box.row(align=True)
+        row.label(text="Decimate Values:")
 
-class OBJECT_OT_CreateLODsQuadrangulate(bpy.types.Operator):
-    bl_idname = "object.create_lods_quadrangulate"
-    bl_label = "Create LODs (for Quadrangulate object)"
+        # Check the preset and enable/disable the properties accordingly
+        is_custom_preset = context.scene.lod_panel.preset == 'CUSTOM'
+
+        for i in range(4):
+            row = box.row(align=True)
+            row.enabled = is_custom_preset
+            row.prop(context.scene.lod_panel, f"decimate_values", index=i, text=f"LOD{i + 1}", emboss=is_custom_preset)
+
+        row = box.row(align=True)
+        row.scale_y = 2.0
+        row.operator("object.create_lods")
+        
+class OBJECT_OT_CreateLODs(bpy.types.Operator):
+    bl_idname = "object.create_lods"
+    bl_label = "Create LODs"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        if not context.selected_objects:
-            self.report({'INFO'}, 'No object selected. Please select an object.')
-            return {'CANCELLED'}
-
-        original_obj = context.active_object
-        original_obj.name = 'LOD0'
-
-        for i, ratio in enumerate([0.5, 0.3, 0.2, 0.1]):
-            bpy.ops.object.duplicate(linked=False, mode='TRANSLATION')
-            new_obj = context.active_object
-
-            for modifier in new_obj.modifiers:
-                new_obj.modifiers.remove(modifier)
-
-            decimate_modifier = new_obj.modifiers.new(name=f'Decimate_{i}', type='DECIMATE')
-            decimate_modifier.ratio = ratio
-
-            weighted_normal_modifier = new_obj.modifiers.new(name=f'Weighted_Normal_{i}', type='WEIGHTED_NORMAL')
-            weighted_normal_modifier.keep_sharp = True
-            weighted_normal_modifier.use_face_influence = True
-            
-            new_obj.name = f'LOD{i + 1}'
-
-        return {'FINISHED'}
-
-class OBJECT_OT_CreateLODsTriangulate(bpy.types.Operator):
-    bl_idname = "object.create_lods_triangulate"
-    bl_label = "Create LODs (for Triangulate object)"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    def execute(self, context):
-        if not context.selected_objects:
-            self.report({'INFO'}, 'No object selected. Please select an object.')
-            return {'CANCELLED'}
-
-        original_obj = context.active_object
-        original_obj.name = 'LOD0'
-
-        for i, ratio in enumerate([0.8, 0.6, 0.4, 0.2]):
-            bpy.ops.object.duplicate(linked=False, mode='TRANSLATION')
-            new_obj = context.active_object
-
-            for modifier in new_obj.modifiers:
-                new_obj.modifiers.remove(modifier)
-
-            decimate_modifier = new_obj.modifiers.new(name=f'Decimate_{i}', type='DECIMATE')
-            decimate_modifier.ratio = ratio
-
-            weighted_normal_modifier = new_obj.modifiers.new(name=f'Weighted_Normal_{i}', type='WEIGHTED_NORMAL')
-            weighted_normal_modifier.keep_sharp = True
-            weighted_normal_modifier.use_face_influence = True
-            
-            new_obj.name = f'LOD{i + 1}'
-
-        return {'FINISHED'}
+        lod_panel = context.scene.lod_panel
+        ratios = lod_panel.decimate_values
+        return lod_panel.create_lods(context, ratios, 'quadrangulate')
 
 def menu_func(self, context):
-    self.layout.operator(OBJECT_OT_CreateLODsQuadrangulate.bl_idname)
+    self.layout.operator(OBJECT_OT_CreateLODs.bl_idname)
 
 def register():
-    bpy.utils.register_class(OBJECT_OT_CreateLODsQuadrangulate)
-    bpy.utils.register_class(OBJECT_OT_CreateLODsTriangulate)
+    bpy.utils.register_class(LODPanelProperties)
+    bpy.types.Scene.lod_panel = bpy.props.PointerProperty(type=LODPanelProperties)
+    bpy.utils.register_class(OBJECT_OT_CreateLODs)
     bpy.utils.register_class(OBJECT_PT_DayZLODToolsPanel)
 
 def unregister():
-    bpy.utils.unregister_class(OBJECT_OT_CreateLODsQuadrangulate)
-    bpy.utils.unregister_class(OBJECT_OT_CreateLODsTriangulate)
+    bpy.utils.unregister_class(LODPanelProperties)
+    del bpy.types.Scene.lod_panel
+    bpy.utils.unregister_class(OBJECT_OT_CreateLODs)
     bpy.utils.unregister_class(OBJECT_PT_DayZLODToolsPanel)
 
 if __name__ == "__main__":
