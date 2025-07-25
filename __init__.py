a76f8d2dc55af94b92099ba2f7a39b1fe0a2e240
SXDIST <nightmoon.yt228@gmail.com>
2025-07-25 20:25:21 +0300

    Refactor registration and loading of LOD properties in __init__.py; add ensure_lodnoshadow function for scene initialization

--- __init__.py
+++ __init__.py
@@ -9,33 +9,65 @@ bl_info = {
 import bpy
 from . import panel, operators, properties
 
+def ensure_lodnoshadow(scene):
+    resolution_lods = scene.a3obe_resolution_lods
+    if not any(p.name == 'lodnoshadow' for p in resolution_lods.named_properties):
+        prop = resolution_lods.named_properties.add()
+        prop.name = 'lodnoshadow'
+        prop.value = '1'
+
+    geometry_lod = scene.a3obe_geometry_lod
+    if not any(p.name == 'autocenter' for p in geometry_lod.named_properties):
+        prop = geometry_lod.named_properties.add()
+        prop.name = 'autocenter'
+        prop.value = '0'
+
+def on_scene_load(dummy):
+    for scene in bpy.data.scenes:
+        ensure_lodnoshadow(scene)
+
 def register():
-    bpy.utils.register_class(properties.A3OBE_PG_NamedProperty)
-    bpy.utils.register_class(properties.A3OBE_PG_ResolutionLODs)
-    bpy.utils.register_class(properties.A3OBE_PG_GeometryLOD)
-    bpy.utils.register_class(operators.A3OBE_OT_GenerateLODs)
-    bpy.utils.register_class(operators.A3OBE_OT_AddNamedProperty_Resolution)
-    bpy.utils.register_class(operators.A3OBE_OT_AddNamedProperty_Geometry)
-    bpy.utils.register_class(operators.A3OBE_OT_RemoveNamedProperty_Resolution)
-    bpy.utils.register_class(operators.A3OBE_OT_RemoveNamedProperty_Geometry)
-    bpy.utils.register_class(panel.A3OBE_PT_AutoLOD)
+    for cls in (
+        properties.A3OBE_PG_NamedProperty,
+        properties.A3OBE_PG_ResolutionLODs,
+        properties.A3OBE_PG_GeometryLOD,
+        operators.A3OBE_OT_GenerateLODs,
+        operators.A3OBE_OT_AddNamedProperty_Resolution,
+        operators.A3OBE_OT_AddNamedProperty_Geometry,
+        operators.A3OBE_OT_RemoveNamedProperty_Resolution,
+        operators.A3OBE_OT_RemoveNamedProperty_Geometry,
+        panel.A3OBE_PT_AutoLOD,
+    ):
+        try:
+            bpy.utils.register_class(cls)
+        except ValueError:
+            pass
 
     bpy.types.Scene.a3obe_resolution_lods = bpy.props.PointerProperty(type=properties.A3OBE_PG_ResolutionLODs)
     bpy.types.Scene.a3obe_geometry_lod = bpy.props.PointerProperty(type=properties.A3OBE_PG_GeometryLOD)
 
+    if on_scene_load not in bpy.app.handlers.load_post:
+        bpy.app.handlers.load_post.append(on_scene_load)
+
 def unregister():
-    bpy.utils.unregister_class(panel.A3OBE_PT_AutoLOD)
-    bpy.utils.unregister_class(operators.A3OBE_OT_GenerateLODs)
-    bpy.utils.unregister_class(operators.A3OBE_OT_AddNamedProperty_Resolution)
-    bpy.utils.unregister_class(operators.A3OBE_OT_AddNamedProperty_Geometry)
-    bpy.utils.unregister_class(operators.A3OBE_OT_RemoveNamedProperty_Resolution)
-    bpy.utils.unregister_class(operators.A3OBE_OT_RemoveNamedProperty_Geometry)
-    bpy.utils.unregister_class(properties.A3OBE_PG_NamedProperty)
-    bpy.utils.unregister_class(properties.A3OBE_PG_ResolutionLODs)
-    bpy.utils.unregister_class(properties.A3OBE_PG_GeometryLOD)
+    if on_scene_load in bpy.app.handlers.load_post:
+        bpy.app.handlers.load_post.remove(on_scene_load)
 
     del bpy.types.Scene.a3obe_resolution_lods
     del bpy.types.Scene.a3obe_geometry_lod
 
-if __name__ == "__main__":
-    register()
+    for cls in reversed((
+        panel.A3OBE_PT_AutoLOD,
+        operators.A3OBE_OT_RemoveNamedProperty_Geometry,
+        operators.A3OBE_OT_RemoveNamedProperty_Resolution,
+        operators.A3OBE_OT_AddNamedProperty_Geometry,
+        operators.A3OBE_OT_AddNamedProperty_Resolution,
+        operators.A3OBE_OT_GenerateLODs,
+        properties.A3OBE_PG_GeometryLOD,
+        properties.A3OBE_PG_ResolutionLODs,
+        properties.A3OBE_PG_NamedProperty,
+    )):
+        try:
+            bpy.utils.unregister_class(cls)
+        except RuntimeError:
+            pass
