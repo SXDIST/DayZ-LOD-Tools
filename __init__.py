e609cc91c1f173ea30faffc1783901f3a90b714c
SXDIST <nightmoon.yt228@gmail.com>
2025-07-25 08:42:52 +0300

    Refactor registration of properties and operators in __init__.py; update descriptions and remove unused memory LOD property

--- __init__.py
+++ __init__.py
@@ -1,44 +1,41 @@
 bl_info = {
-    "name" : "Arma 3 Object Builder Extensions",
-    "description" : "A set of usefull extensions for Arma 3 Object Builder addon",
-    "author" : "Mikk, SXDIST",
-    "blender" : (4, 5, 0),
-    "category" : "3D View"
+    "name": "Arma 3 Object Builder Extensions",
+    "description": "Extensions for Arma 3 Object Builder",
+    "author": "Mikk, SXDIST",
+    "blender": (4, 5, 0),
+    "category": "3D View"
 }
 
 import bpy
 from . import panel, operators, properties
 
 def register():
-    bpy.utils.register_class(panel.A3OBE_PT_AutoLOD)
-    bpy.utils.register_class(operators.A3OBE_OT_GenerateLODs)
-    bpy.utils.register_class(operators.A3OBE_OT_AddNamedProperty)
-    bpy.utils.register_class(operators.A3OBE_OT_RemoveNamedProperty)
     bpy.utils.register_class(properties.A3OBE_PG_NamedProperty)
     bpy.utils.register_class(properties.A3OBE_PG_ResolutionLODs)
     bpy.utils.register_class(properties.A3OBE_PG_GeometryLOD)
-    bpy.utils.register_class(properties.A3OBE_PG_MemoryLOD)
+    bpy.utils.register_class(operators.A3OBE_OT_GenerateLODs)
+    bpy.utils.register_class(operators.A3OBE_OT_AddNamedProperty_Resolution)
+    bpy.utils.register_class(operators.A3OBE_OT_AddNamedProperty_Geometry)
+    bpy.utils.register_class(operators.A3OBE_OT_RemoveNamedProperty_Resolution)
+    bpy.utils.register_class(operators.A3OBE_OT_RemoveNamedProperty_Geometry)
+    bpy.utils.register_class(panel.A3OBE_PT_AutoLOD)
 
-    bpy.types.Scene.a3obe_resolution_lods = properties.PointerProperty(type=properties.A3OBE_PG_ResolutionLODs)
-    bpy.types.Scene.a3obe_geometry_lod = properties.PointerProperty(type=properties.A3OBE_PG_GeometryLOD)
-    bpy.types.Scene.a3obe_memory_lod = properties.PointerProperty(type=properties.A3OBE_PG_MemoryLOD)
+    bpy.types.Scene.a3obe_resolution_lods = bpy.props.PointerProperty(type=properties.A3OBE_PG_ResolutionLODs)
+    bpy.types.Scene.a3obe_geometry_lod = bpy.props.PointerProperty(type=properties.A3OBE_PG_GeometryLOD)
 
 def unregister():
     bpy.utils.unregister_class(panel.A3OBE_PT_AutoLOD)
     bpy.utils.unregister_class(operators.A3OBE_OT_GenerateLODs)
-    bpy.utils.unregister_class(operators.A3OBE_OT_AddNamedProperty)
-    bpy.utils.unregister_class(operators.A3OBE_OT_RemoveNamedProperty)
+    bpy.utils.unregister_class(operators.A3OBE_OT_AddNamedProperty_Resolution)
+    bpy.utils.unregister_class(operators.A3OBE_OT_AddNamedProperty_Geometry)
+    bpy.utils.unregister_class(operators.A3OBE_OT_RemoveNamedProperty_Resolution)
+    bpy.utils.unregister_class(operators.A3OBE_OT_RemoveNamedProperty_Geometry)
     bpy.utils.unregister_class(properties.A3OBE_PG_NamedProperty)
     bpy.utils.unregister_class(properties.A3OBE_PG_ResolutionLODs)
     bpy.utils.unregister_class(properties.A3OBE_PG_GeometryLOD)
-    bpy.utils.unregister_class(properties.A3OBE_PG_MemoryLOD)
 
-    if hasattr(bpy.types.Scene, 'a3obe_resolution_lods'):
-        del bpy.types.Scene.a3obe_resolution_lods
-    if hasattr(bpy.types.Scene, 'a3obe_geometry_lod'):
-        del bpy.types.Scene.a3obe_geometry_lod
-    if hasattr(bpy.types.Scene, 'a3obe_memory_lod'):
-        del bpy.types.Scene.a3obe_memory_lod
+    del bpy.types.Scene.a3obe_resolution_lods
+    del bpy.types.Scene.a3obe_geometry_lod
 
 if __name__ == "__main__":
     register()
