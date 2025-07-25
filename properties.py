73f74de7986d0f128a32b2822844670ece2a600b
SXDIST <nightmoon.yt228@gmail.com>
2025-07-25 08:43:13 +0300

    Enhance property descriptions in properties.py; improve clarity for LOD generation settings and add geometry LOD support

--- properties.py
+++ properties.py
@@ -3,29 +3,31 @@ from bpy.props import BoolProperty, StringProperty, EnumProperty, FloatVectorPro
 from bpy.types import PropertyGroup
 
 class A3OBE_PG_NamedProperty(PropertyGroup):
-    name: StringProperty(name="Name", default="")
-    value: StringProperty(name="Value", default="")
+    name: StringProperty(name="Name", description="Name of the custom property", default="")
+    value: StringProperty(name="Value", description="Value of the custom property", default="")
 
 class A3OBE_PG_ResolutionLODs(PropertyGroup):
-    active: BoolProperty(name='Generate Resolution LODs', default=True)
-    lod_prefix: StringProperty(name='', description='Resolution LOD prefix', default='resolution_lod_')
-    first_lod: EnumProperty(description='First LOD', items=[('LOD0', 'LOD 0', ''), ('LOD1', 'LOD 1', '')], default='LOD1')
-    preset: EnumProperty(description='Preset', items=[('CUSTOM', 'Custom', ''), ('TRIS', 'Tris', ''), ('QUADS', 'Quads', '')], default='QUADS')
-    custom_decimate_values: FloatVectorProperty(size=4, min=0.0, max=1.0, default=(0.75, 0.50, 0.25, 0.10))
-    tris_decimate_values: FloatVectorProperty(size=4, min=0.0, max=1.0, default=(0.80, 0.60, 0.40, 0.20))
-    quads_decimate_values: FloatVectorProperty(size=4, min=0.0, max=1.0, default=(0.50, 0.30, 0.20, 0.10))
-    named_properties: CollectionProperty(type=A3OBE_PG_NamedProperty)
+    active: BoolProperty(name="Generate Resolution LODs", description="Enable generation of resolution LODs", default=True)
+    lod_prefix: StringProperty(name="Prefix", description="Prefix for resolution LOD object names", default="resolution_lod_")
+    first_lod: EnumProperty(items=[('LOD0', "LOD 0", "Start with LOD 0", 'OUTLINER_OB_MESH', 0),
+                                  ('LOD1', "LOD 1", "Start with LOD 1", 'OUTLINER_OB_MESH', 1)],
+                           default='LOD1', description="Select the starting LOD level")
+    preset: EnumProperty(items=[('CUSTOM', "Custom", "Use custom decimate values", 'MODIFIER', 0),
+                               ('TRIS', "Tris", "Use tris-based decimate values", 'MODIFIER', 1),
+                               ('QUADS', "Quads", "Use quads-based decimate values", 'MODIFIER', 2)],
+                        default='QUADS', description="Choose a preset for decimation ratios")
+    custom_decimate_values: FloatVectorProperty(size=4, min=0.0, max=1.0, default=(0.75, 0.50, 0.25, 0.10),
+                                               description="Custom decimate ratios for LODs 0-3")
+    tris_decimate_values: FloatVectorProperty(size=4, min=0.0, max=1.0, default=(0.80, 0.60, 0.40, 0.20),
+                                             description="Tris-based decimate ratios for LODs 0-3")
+    quads_decimate_values: FloatVectorProperty(size=4, min=0.0, max=1.0, default=(0.50, 0.30, 0.20, 0.10),
+                                              description="Quads-based decimate ratios for LODs 0-3")
+    named_properties: CollectionProperty(type=A3OBE_PG_NamedProperty, description="List of custom properties for resolution LODs")
 
 class A3OBE_PG_GeometryLOD(PropertyGroup):
-    active: BoolProperty(name='Generate Geometry LOD [WIP]', default=False)
-    lod_name: StringProperty(name='', description='Geometry LOD name', default='geometry_lod')
-    convex_hull_mesh: BoolProperty(name='Create Convex Hull mesh', default=True)
-    autocenter_property: BoolProperty(name='Disable "autocenter = 0" property', default=True)
-
-class A3OBE_PG_MemoryLOD(PropertyGroup):
-    active: BoolProperty(name='Generate Memory LOD [WIP]', default=False)
-    lod_name: StringProperty(name='', description='Memory LOD name', default='memory_lod')
-    create_boundingbox_min_point: BoolProperty(name='Create BoundingBox_Min point', default=True)
-    create_boundingbox_max_point: BoolProperty(name='Create BoundingBox_Max point', default=True)
-    create_invview_point: BoolProperty(name='Create InvView point', default=True)
-    autocenter_property: BoolProperty(name='Disable "autocenter = 0" property', default=True)
+    active: BoolProperty(name="Generate Geometry LOD", description="Enable generation of geometry LOD", default=False)
+    lod_name: StringProperty(name="Name", description="Name of the geometry LOD object", default="geometry_lod")
+    geometry_type: EnumProperty(items=[('BOX', "Box", "Generate a bounding box", 'MESH_CUBE', 0),
+                                      ('NONE', "None", "Create empty object with properties", 'BLANK1', 1)],
+                                default='BOX', description="Choose the type of geometry LOD to generate")
+    named_properties: CollectionProperty(type=A3OBE_PG_NamedProperty, description="List of custom properties for geometry LOD")
