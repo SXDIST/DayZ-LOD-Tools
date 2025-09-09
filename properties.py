651af09a55a1b895192bb900c06fd83bdd0f6da9
Arima Kurosawa <nightmoon.yt228@gmail.com>
2025-09-09 16:35:34 +0300

    - Add A3OBE_PG_MemoryLOD with Standard Points (invview, bounding_box, radius, center) - Add A3OBE_PG_FireGeometryLOD with quality slider (1-10) - Add A3OBE_PG_ViewGeometryLOD with customizable naming - Update default Resolution LOD prefix to 'LOD ' - Update default Geometry LOD name to 'Geometry' - Remove Weapons Points properties (bolt_axis, bullet_eject, eye_ads) - Add IntProperty import for quality slider - Simplify Memory LOD to focus on standard points only"

--- properties.py
+++ properties.py
@@ -1,5 +1,5 @@
 import bpy
-from bpy.props import BoolProperty, StringProperty, EnumProperty, FloatVectorProperty, CollectionProperty, PointerProperty
+from bpy.props import BoolProperty, StringProperty, EnumProperty, FloatVectorProperty, CollectionProperty, PointerProperty, IntProperty
 from bpy.types import PropertyGroup
 
 class A3OBE_PG_NamedProperty(PropertyGroup):
@@ -8,7 +8,7 @@ class A3OBE_PG_NamedProperty(PropertyGroup):
 
 class A3OBE_PG_ResolutionLODs(PropertyGroup):
     active: BoolProperty(name="Generate Resolution LODs", description="Enable generation of resolution LODs", default=True)
-    lod_prefix: StringProperty(name="Prefix", description="Prefix for resolution LOD object names", default="resolution_lod_")
+    lod_prefix: StringProperty(name="Prefix", description="Prefix for resolution LOD object names", default="LOD ")
     first_lod: EnumProperty(items=[('LOD0', "LOD 0", "Start with LOD 0", 'OUTLINER_OB_MESH', 0),
                                   ('LOD1', "LOD 1", "Start with LOD 1", 'OUTLINER_OB_MESH', 1)],
                            default='LOD1', description="Select the starting LOD level")
@@ -26,9 +26,29 @@ class A3OBE_PG_ResolutionLODs(PropertyGroup):
 
 class A3OBE_PG_GeometryLOD(PropertyGroup):
     active: BoolProperty(name="Generate Geometry LOD", description="Enable generation of geometry LOD", default=True)
-    lod_name: StringProperty(name="Name", description="Name of the geometry LOD object", default="geometry_lod")
+    lod_name: StringProperty(name="Name", description="Name of the geometry LOD object", default="Geometry")
     geometry_type: EnumProperty(items=[('BOX', "Box", "Generate a single bounding box", 'MESH_CUBE', 0),
-                                      ('MULTIPLE_BOXES', "Multiple Boxes", "Generate bounding boxes for each loose part", 'MESH_CUBE', 1),
-                                      ('NONE', "None", "Create empty object with properties", 'EMPTY_DATA', 2)],
+                                      ('NONE', "None", "Create empty object with properties", 'EMPTY_DATA', 1)],
                                 default='BOX', description="Choose the type of geometry LOD to generate")
     named_properties: CollectionProperty(type=A3OBE_PG_NamedProperty, description="List of custom properties for geometry LOD")
+
+class A3OBE_PG_MemoryLOD(PropertyGroup):
+    active: BoolProperty(name="Generate Memory LOD", description="Enable generation of memory LOD", default=False)
+    
+    # Standard Points
+    invview_point: BoolProperty(name="Inview Point", description="Add invview point to memory", default=True)
+    bounding_box_points: BoolProperty(name="Bounding Box Points", description="Add boundingbox_min and boundingbox_max points", default=True)
+    radius_point: BoolProperty(name="Radius Point", description="Add ce_radius point to memory", default=False)
+    center_point: BoolProperty(name="Center Point", description="Add ce_center point to memory", default=False)
+    
+    named_properties: CollectionProperty(type=A3OBE_PG_NamedProperty, description="List of custom properties for memory LOD")
+
+class A3OBE_PG_FireGeometryLOD(PropertyGroup):
+    active: BoolProperty(name="Generate Fire Geometry LOD", description="Enable generation of fire geometry LOD", default=False)
+    quality: IntProperty(name="Fire Geometry Quality", description="1 = low polygon count, 10 = high polygon count", min=1, max=10, default=2)
+    named_properties: CollectionProperty(type=A3OBE_PG_NamedProperty, description="List of custom properties for fire geometry LOD")
+
+class A3OBE_PG_ViewGeometryLOD(PropertyGroup):
+    active: BoolProperty(name="Generate View Geometry LOD", description="Enable generation of view geometry LOD", default=False)
+    lod_name: StringProperty(name="Name", description="Name of the view geometry LOD object", default="View Geometry")
+    named_properties: CollectionProperty(type=A3OBE_PG_NamedProperty, description="List of custom properties for view geometry LOD")
